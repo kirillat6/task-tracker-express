8e2ba96f2545bbaa7d4eca6487db7649e5061f7b
Kirill Tihonov <taylormr047@gmail.com>
2026-02-22 22:47:07 +0300

    Add express-authorization

--- src/config.ts
+++ src/config.ts
@@ -1,2 +1,4 @@
 export const PORT = +(process.env.PORT || 80);
 export const SQLITE_PATH = process.env.SQLITE_PATH || './db.db';
+export const ADMIN_LOGIN = process.env.ADMIN_LOGIN || 'admin';
+export const ADMIN_PASSWORD = process.env.ADMIN_PASSWORD || 'admin';
--- src/index.ts
+++ src/index.ts
@@ -1,13 +1,20 @@
 import express from 'express';
-import { PORT } from './config';
+import { ADMIN_LOGIN, ADMIN_PASSWORD, PORT } from './config';
 import { cardsRouter } from './routers/cards.router';
 import { createTables } from './database/create-tables';
+import basicAuth from 'express-basic-auth';
 
 async function run() {
 	await createTables();
 
 	const app = express();
 	app.use(express.json());
+	app.use(
+		basicAuth({
+			users: { [ADMIN_LOGIN]: ADMIN_PASSWORD },
+			challenge: true,
+		}),
+	);
 	app.get('/', (request, response) => {
 		response.send('OK');
 	});
